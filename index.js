2de32937e717ecc35d5a2ae9077d09c35c540b48
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-09-16 22:30:48 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// function more() {
+//     document.querySelector(".count").innerHTML++;
+//   }
+
+
+
+var clicks = 0;
+function increment(){
+     clicks = clicks + 1;
+        cookies.innerHTML = clicks;
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
